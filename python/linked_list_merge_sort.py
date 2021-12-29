d476d06a7ad9a000a0b9a25cf3af094b9de17ecd
MizouziE <s.anglin@live.co.uk>
2021-12-29 11:26:42 +0000

    trying to start but the keyboard is going nuts

--- python/linked_list_merge_sort.py
+++ python/linked_list_merge_sort.py
@@ -1,5 +1,7 @@
 from linked_list import SinglyLinkedList
 
-l = SinglyLinkedList()
-l.add(42)
-print(l)
+def merge_sort(linked_list):
+    """
+    Sorts a linked list in ascending order
+    - Recursively divide the linked list into sublists containing a single node
+    - Repeatedly merge the sublists to produce sorted sublists until one remains
